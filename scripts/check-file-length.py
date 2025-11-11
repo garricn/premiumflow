f7a3dbf0bf4d80c04c956196fe80210b789da34c
Garric G. Nahapetian <garricn@icloud.com>
2025-11-10 20:11:23 -0800

    chore: harden file-length check

--- scripts/check-file-length.py
+++ scripts/check-file-length.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """A script to check file length."""
 import argparse
 import sys
-from typing import Sequence
+from typing import Optional, Sequence
 
 
-def main(argv: Sequence[str] | None = None) -> int:
+def main(argv: Optional[Sequence[str]] = None) -> int:
     """Check file length."""
     parser = argparse.ArgumentParser()
     parser.add_argument("filenames", nargs="*", help="Filenames to check.")
@@ -25,18 +25,10 @@ def main(argv: Sequence[str] | None = None) -> int:
             with open(filename, "r") as f:
                 lines = f.readlines()
                 has_ignore_comment = any("# file-length-ignore" in line for line in lines[:5])
-                
-                if len(lines) > args.max_lines:
-                    if not has_ignore_comment:
-                        print(
-                            f"{filename}: file is too long "
-                            f"({len(lines)} > {args.max_lines} lines)"
-                        )
-                        retv = 1
-                elif has_ignore_comment:
+
+                if len(lines) > args.max_lines and not has_ignore_comment:
                     print(
-                        f"{filename}: superfluous # file-length-ignore comment; "
-                        f"file is {len(lines)} lines (max {args.max_lines})"
+                        f"{filename}: file is too long " f"({len(lines)} > {args.max_lines} lines)"
                     )
                     retv = 1
         except Exception as e:
